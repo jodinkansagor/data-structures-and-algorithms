e973e876393b70188bbe54753761c7e42e90a6fe
Jodi N Kansagor <joedvice@gmail.com>
2019-12-07 10:49:10 -0800

    passed test 1, can make a new empty list

--- code-challenges/challenge-04/__tests__/linked-list.test.js
+++ code-challenges/challenge-04/__tests__/linked-list.test.js
@@ -2,7 +2,31 @@ const { LinkedList, Node } = require('../linked-list');
 
 describe('linked list tests', () => {
   it('instantiates a new list', () => {
-    const newList = new LinkedList();
-    
-  })
+    const newList = new LinkedList;
+    expect(newList).toBeNull;
+  });
+
+  // it('inserts into the list', () => {
+
+  // });
+
+  // it('the head property points to the first node', () => {
+
+  // });
+
+  // it('can properly insert multiple nodes into the list', () => {
+
+  // });
+
+  // it('has a method that returns true if the value is found', () => {
+
+  // });
+
+  // it('has a method that returns false when looking for a value that is not in the list', () => {
+
+  // });
+
+  // it('can properly return a collection of all the values in a string', () => {
+
+  // });
 })
