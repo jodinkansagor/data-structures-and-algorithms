108d6c466076248eca72bb84508a44b5b7ac5d31
Jodi N Kansagor <joedvice@gmail.com>
2019-12-07 09:38:50 -0800

    commit to jump off branch

--- code-challenges/challenge-02/__test__/array-shift.test.js
+++ code-challenges/challenge-02/__test__/array-shift.test.js
@@ -1,7 +1,7 @@
 const { insertShiftArray } = require('../array.shift');
 
 describe('shift array', () => {
-    it.skip('reverses any array that you give it', () => {
+    it.skip('adds an element to an array in the middle', () => {
         expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
         expect(insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
     });
--- code-challenges/challenge-03/array-binary-search.js
+++ code-challenges/challenge-03/array-binary-search.js
@@ -1,12 +1,11 @@
 const binarySearch = (array, searchKey) => {
-    let indexWeAreLookingFor = 0;
     for(let i = 0; i < array.length; i++) {
         if(array[i] === searchKey) {
-            return indexWeAreLookingFor;
+            return i;
+        } else {
+            return -1;
         }
-        indexWeAreLookingFor++;
     }
-    return -1;
 };
 
 module.exports = { binarySearch };
