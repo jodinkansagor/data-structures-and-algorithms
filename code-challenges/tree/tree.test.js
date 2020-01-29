a1909a95376b4c5dd32ff658995ac961229fe3ad
Jodi N Kansagor <joedvice@gmail.com>
2020-01-28 19:11:00 -0800

    test 1 passes

--- code-challenges/tree/tree.test.js
+++ code-challenges/tree/tree.test.js
@@ -2,7 +2,8 @@ const { Tree } = require ('./tree.js');
 
 describe('binary tree', () => {
   it('instatiates an empty tree', ()  => {
+    const tree = new Tree();
+    expect(tree).tobeNull;
+  });
+});
 
-  }
-
-})
