2cea75b3a62557602c5141a8a5a72b6cb6e2fb3e
Jodi N Kansagor <joedvice@gmail.com>
2020-01-28 19:13:50 -0800

    test 2 passes

--- code-challenges/tree/tree.test.js
+++ code-challenges/tree/tree.test.js
@@ -5,5 +5,11 @@ describe('binary tree', () => {
     const tree = new Tree();
     expect(tree).tobeNull;
   });
+
+  it('instantiates a tree with a single root node', () => {
+    const tree = new Tree();
+    tree.addValue(4);
+    expect(tree.root).toEqual({ 'left': null, 'right': null, 'value': 4 });
+  });
 });
 
